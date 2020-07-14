7d80c8f6c63dd60ba2d4ed4d4d8f6b3535961789
Dusch4593 <brandondusch@gmail.com>
2020-07-14 00:02:07 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const handleClick = (e) => {
+  this.props.handleClick()
+}
+
+const SimplerComponent = ({handleClick}) => (<div onClick={handleClick}>I am just happy</div>)
+
+export default SimplerComponent
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const showCongrats = () => {
+  console.log("Congrats!")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => showCongrats} />
   </div>,
   document.getElementById('root')
 );
